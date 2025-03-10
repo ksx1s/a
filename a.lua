386a30c4622f95087884957881189c3a5410e4ab
ksx <discord.ksx@gmail.com>
2025-03-10 02:57:01 -0300

    Update a.lua

--- a.lua
+++ a.lua
@@ -1,4 +1,5 @@
 local tagsList = {
-    Developer = {584295859, color = Color3.fromRGB(0, 0, 0)}
+    Developer = {584295859, color = Color3.fromRGB(0, 0, 0)},
+    Fofinha = {4327261423, color = Color3.fromRGB(255, 203, 219)}
 }
 return tagsList
