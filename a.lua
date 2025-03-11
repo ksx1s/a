29a4c4e3047980a09735477ee9ad89294e7de6a9
ksx <discord.ksx@gmail.com>
2025-03-11 05:17:31 -0300

    Update a.lua

--- a.lua
+++ a.lua
@@ -1,8 +1,8 @@
 local tagsList = {
-    ["Developer"] = {584295859, color = Color3.fromRGB(129, 0, 2)},
-    ["Friend"] = {2894363622, 743607985, color = Color3.fromRGB(255, 255, 0)},
-    ["Fofinha"] = {4327261423, color = Color3.fromRGB(255, 203, 219)},
-    ["soltaooruam"] = {3558520129, color = Color3.fromRGB(255, 203, 219)},
-    ["soltaoboladin"] = {1442818339, color = Color3.fromRGB(255, 203, 219)}
+    ["Developer"] = {ids = {584295859}, color = Color3.fromRGB(129, 0, 2)},
+    ["Friend"] = {ids = {2894363622, 743607985}, color = Color3.fromRGB(255, 255, 0)},
+    ["Fofinha"] = {ids = {4327261423}, color = Color3.fromRGB(255, 203, 219)},
+    ["soltaooruam"] = {ids = {3558520129}, color = Color3.fromRGB(255, 203, 219)},
+    ["soltaoboladin"] = {ids = {1442818339}, color = Color3.fromRGB(255, 203, 219)}
 }
 return tagsList
