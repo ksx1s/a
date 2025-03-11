f30e670ed52602cb12cf94e9c8630923998cc7d5
ksx <discord.ksx@gmail.com>
2025-03-11 00:55:25 -0300

    Update a.lua

--- a.lua
+++ a.lua
@@ -1,6 +1,7 @@
 local tagsList = {
     Developer = {584295859, color = Color3.fromRGB(129, 0, 2)},
     Fofinha = {4327261423, color = Color3.fromRGB(255, 203, 219)},
-    soltaooruam = {3558520129, color = Color3.fromRGB(255, 203, 219)}
+    soltaooruam = {3558520129, color = Color3.fromRGB(255, 203, 219)},
+    soltaoboladin = {1442818339, color = Color3.fromRGB(255, 203, 219)}
 }
 return tagsList
