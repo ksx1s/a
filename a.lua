d284705b2f69104c9bee72886186e8747391ca9d
ksx <discord.ksx@gmail.com>
2025-03-11 22:38:06 -0300

    Update a.lua

--- a.lua
+++ a.lua
@@ -2,6 +2,7 @@ local tagsList = {
     ["Developer"] = {ids = {584295859}, color = Color3.fromRGB(129, 0, 2)},
     ["Friend"] = {ids = {2894363622, 743607985}, color = Color3.fromRGB(255, 255, 0)},
     ["Fofinha"] = {ids = {4327261423}, color = Color3.fromRGB(255, 203, 219)},
+    ["hater do ksx"] = {ids = {3533749155}, color = Color3.fromRGB(1, 75, 160)},
     ["soltaooruam"] = {ids = {3558520129}, color = Color3.fromRGB(255, 203, 219)},
     ["soltaoboladin"] = {ids = {1442818339}, color = Color3.fromRGB(255, 203, 219)}
 }
