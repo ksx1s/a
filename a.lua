0605b510a4d3f514a02733781150ccafb67b758a
ksx <discord.ksx@gmail.com>
2025-03-12 09:04:19 -0300

    Update a.lua

--- a.lua
+++ a.lua
@@ -1,7 +1,8 @@
 local tagsList = {
+    ["Dono do roblox"] = {ids = {8046916119}, color = Color3.fromRGB(255, 0, 0)},
     ["Developer"] = {ids = {584295859}, color = Color3.fromRGB(129, 0, 2)},
     ["Friend"] = {ids = {2894363622, 743607985}, color = Color3.fromRGB(255, 255, 0)},
-    ["namorado do ksx"] = {ids = {5437370852}, color = Color3.fromRGB(160, 32, 240)},
+    ["Dono do mushyo"] = {ids = {5437370852}, color = Color3.fromRGB(160, 32, 240)},
     ["Fofinha"] = {ids = {4327261423}, color = Color3.fromRGB(255, 203, 219)},
     ["hater do ksx"] = {ids = {3533749155}, color = Color3.fromRGB(1, 75, 160)},
     ["soltaooruam"] = {ids = {3558520129}, color = Color3.fromRGB(255, 203, 219)},
