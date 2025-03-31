0f8c067cd7cde3749e1108ba6ed9d1e24a6fdbe5
ksx <discord.ksx@gmail.com>
2025-03-31 00:41:16 -0300

    Update a.lua

--- a.lua
+++ a.lua
@@ -17,6 +17,7 @@ local tagsList = {
     ["broxa aos 21"] = {ids = {5022130490}, color = Color3.fromRGB(255, 203, 219)},
     -- ["Veterano"] = {ids = {}, color = Color3.fromRGB(1, 75, 160)},
     --
+    ["Flex"] = {ids = {796931256}, color = Color3.fromRGB(1, 75, 160)},
     ["risp ♥"] = {ids = {2247758467, 7837117044}, color = Color3.fromRGB(255, 255, 0)},
     ["psilo ♥"] = {ids = {7635173078, 5384166070}, color = Color3.fromRGB(0, 255, 0)}
 }
